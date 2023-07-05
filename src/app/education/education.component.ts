8abfc63da7cc7f75fe4609ed33635d3804029d1c
redmindtech <redmindtech.2020@gmail.com>
2023-07-05 18:36:42 +0530

    Sujith changes

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-education',
@@ -7,9 +10,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducationComponent implements OnInit {
 
+  myControl = new FormControl();
+  options: string[] = ["Adarsh Shayam Dev Private Industrial Training Institute - ITI, Jaunpur , Uttar Pradesh",
+"Advanced Training Institute - ATI, Howrah , West Bengal",
+"Addwait Mahila Audyogik Evam Shilpa Kala Shikshan Kendra, Barabanki , Uttar Pradesh",
+"Advanced Training Institute - ATI, Ludhiana , Punjab",
+"Agra Private Industrial Training Institute - ITI, Agra , Uttar Pradesh",
+"Advanced Training Institute - ATI, Hyderabad , Telangana",
+"AGRAWAL INDUSTRIAL TRAINING INSTITUTE, Jaipur , Rajasthan",
+"Adesh Private Industrial Training Institute - ITI, Pilibhit , Uttar Pradesh",
+"Advanced Training Institute for Electronics and Process Instrumentation, Hyderabad , Telangana",
+"ADFS Private Industrial Training Institute - ITI, Agra , Uttar Pradesh",
+"AECS Pavan Industrial Training Institute, Kolar , Karnataka",
+"Adhaar Private Industrial Training Institute - ITI, Allahabad , Uttar Pradesh",
+"AFWWA Private Industrial Training Institute - ITI, Kanpur Nagar , Uttar Pradesh",
+"Adharshila Private Industrial Training Institute - ITI, Meerut , Uttar Pradesh",
+"Agardi Prasad Yadav Private Industrial Training Institute - ITI, Jaunpur , Uttar Pradesh",
+"Aditya Private Industrial Training Institute - APITI, Mirzapur , Uttar Pradesh",
+"Aditya Private Industrial Training Institute - APITI, Mirzapur , Uttar Pradesh",
+"Agnel Industrial Training Institute,South Goa , Goa",
+"Advance Institute of Information and Computer Technology, Ghaziabad , Uttar Pradesh",
+"Agra Glory Private Industrial Training Institute - ITI, Agra , Uttar Pradesh",
+"Advanced Training Institute - ATI, Ludhiana , Punjab",
+"Agra Private Industrial Training Institute - ITI, Agra , Uttar Pradesh",
+"Advanced Training Institute - ATI, Hyderabad , Telangana",
+"Adarsh Private Industrial Training Institute - ITI, Ghaziabad , Uttar Pradesh",
+"Achiever Industrial Training Institute - ITI, Mahendragarh , Haryana",
+"Adarsh Private Industrial Training Institute - ITI, Bulandshahr , Uttar Pradesh",
+"Acharya Private Industrial Training Institute - ITI, Deoria , Uttar Pradesh",
+"Adarsh Private Industrial Training Institute - ITI, Banda , Uttar Pradesh",
+"Abha Private Industrial Training Institute - ITI,Ambedkar Nagar , Uttar Pradesh",
+"A.S. Private Industrial Training Institute - ITI,Aligarh , Uttar Pradesh",
+"Abhilasha Private Industrial Training Institute - ITI,Ambedkar Nagar , Uttar Pradesh",
+"A.Y.T. Private Industrial Training Institute - ITI,Deoria , Uttar Pradesh",
+"Physically Abhiraji Devi Private Industrial Training Institute - ITI, Basti,Uttar Pradesh",
+"Chandy Industrial Training Centre, Thoothukudi , Tamil Nadu",
+"Swamy Vivekananda Industrial Training Institute, Chennai , Tamil Nadu",
+"Sri Bharathi Industrial School, Vellore , Tamil Nadu",
+"Sri Ramakrishna Advanced Training Institute - SRATI , Coimbatore , Tamil Nadu",
+"Sri Vinayaga Private Industrial Training Institute, Salem , Tamil Nadu",
+
+"St. John's Private Industrial Training Institute - ITI, Tiruchirappalli , Tamil Nadu",];
+
+  filteredOptions!: Observable<string[]>;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
 
   highlightedCard: number = 0;
@@ -56,8 +114,10 @@ export class EducationComponent implements OnInit {
     {value: 'Stick welding', viewValue: 'Stick welding'},
     {value: 'Tungsten Inert Gas (TIG) Welding', viewValue: 'Tungsten Inert Gas (TIG) Welding'},
     {value: 'Welding', viewValue: 'Welding'},
-    
+
   ];
+
+
 
 }
 
@@ -66,6 +126,8 @@ interface education_level {
   value: string;
   viewValue: string;
 }
+
+
 
 interface certificate {
   value: string;
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -69,6 +70,310 @@ export class RegisterComponent implements OnInit {
 
   ];
 
+
+  pokemonControl = new FormControl('');
+  pokemonGroups: PokemonGroup[] = [
+    {
+      name: 'Andaman and Nicobar Islands',
+      pokemon: [
+        {value: 'Port Blair', viewValue: 'Port Blair'},
+      ],
+    },
+    {
+      name: 'Andhra Pradesh',
+      pokemon: [
+        {value: 'Visakhapatnam', viewValue: 'Visakhapatnam'},
+        {value: 'Vijayawada', viewValue: 'Vijayawada'},
+        {value: 'Guntur', viewValue: 'Guntur'},
+        {value: 'Nellore', viewValue: 'Nellore'},
+        {value: 'Tirupati', viewValue: 'Tirupati'},
+
+      ],
+    },
+    {
+      name: 'Arunachal Pradesh',
+
+      pokemon: [
+        {value: 'Tawang', viewValue: 'Tawang'},
+        {value: 'Bhismaknagar', viewValue: 'Bhismaknagar'},
+        {value: 'Pasighat', viewValue: 'Pasighat'},
+        {value: 'Ziro ', viewValue: 'Ziro '},
+        {value: 'Bomdila', viewValue: 'Bomdila'},
+      ],
+    },
+    {
+      name: 'Assam',
+      pokemon: [
+        {value: 'Guwahati', viewValue: 'Guwahati'},
+        {value: 'Tezpur', viewValue: 'Tezpur'},
+        {value: 'Dibrugarh', viewValue: 'Dibrugarh'},
+        {value: 'Silchar', viewValue: 'Silchar'},
+        {value: 'North Lakhimpur', viewValue: 'North Lakhimpur'},
+
+      ],
+    },
+    {
+      name: 'Bihar',
+      pokemon: [
+        {value: 'Gaya', viewValue: 'Gaya'},
+        {value: 'Biharsharif', viewValue: 'Biharsharif'},
+        {value: 'Darbhanga', viewValue: 'Darbhanga'},
+        {value: 'Bhagalpur', viewValue: 'Bhagalpur'},
+      ],
+    },
+    {
+      name: 'Chandigarh',
+      pokemon: [
+        {value: 'Chandigarh', viewValue: 'Chandigarh'},
+
+      ],
+    },
+    {
+      name: 'Chhattisgarh',
+      pokemon: [
+        {value: 'Bilaspur', viewValue: 'Bilaspur'},
+        {value: 'Korba', viewValue: 'Korba'},
+        {value: 'Durg-Bhilainagar', viewValue: 'Durg-Bhilainagar'},
+        {value: 'Raigarh', viewValue: 'Raigarh'},
+        {value: 'Rajnandgaon', viewValue: 'Rajnandgaon'},
+      ],
+    },
+    {
+      name: 'Dadra and Nagar Haveli and Daman and Diu',
+      pokemon: [
+        {value: 'Daman', viewValue: 'Daman'},
+
+      ],
+    },
+    {
+      name: 'Goa',
+      pokemon: [
+        {value: 'Panaji', viewValue: 'Panaji'},
+
+      ],
+    },
+    {
+      name: 'Gujarat',
+      pokemon: [
+        {value: 'Ahmedabad', viewValue: 'Ahmedabad'},
+        {value: 'Surat', viewValue: 'Surat'},
+        {value: 'Rajkot', viewValue: 'Rajkot'},
+        {value: 'Junagadh', viewValue: 'Junagadh'},
+        {value: 'Vadodara', viewValue: 'Vadodara'},
+      ],
+    },
+    {
+      name: 'Haryana',
+      pokemon: [
+        {value: 'Faridabad', viewValue: 'Faridabad'},
+        {value: 'Gurgaon', viewValue: 'Gurgaon'},
+        {value: 'Sonipat', viewValue: 'Sonipat'},
+        {value: 'Panipat', viewValue: 'Panipat'},
+        {value: 'Ambala', viewValue: 'Ambala'},
+      ],
+    },
+    {
+      name: 'Himachal Pradesh',
+      pokemon: [
+        {value: 'Dharamshala', viewValue: 'Dharamshala'},
+        {value: 'Mandi', viewValue: 'Mandi'},
+        {value: 'Solan', viewValue: 'Solan'},
+        {value: 'Bilaspur', viewValue: 'Bilaspur'},
+        {value: 'Chamba', viewValue: 'Chamba'},
+      ],
+    },
+    {
+      name: 'Jharkhand',
+      pokemon: [
+        {value: 'Bokaro Steel City', viewValue: 'Bokaro Steel City'},
+        {value: 'Jamshedpur', viewValue: 'Jamshedpur'},
+        {value: 'Deoghar', viewValue: 'Deoghar'},
+        {value: 'Hazaribagh', viewValue: 'Hazaribagh'},
+        {value: 'Dhanbad', viewValue: 'Dhanbad'},
+      ],
+    },
+    {
+      name: 'Karnataka',
+      pokemon: [
+        {value: 'Mysore', viewValue: 'Mysore'},
+        {value: 'Davangere', viewValue: 'Davangere'},
+        {value: 'Mangalore', viewValue: 'Mangalore'},
+        {value: 'Hubli-Dharwad', viewValue: 'Hubli-Dharwad'},
+        {value: 'Belgaum', viewValue: 'Belgaum'},
+      ],
+    },
+    {
+      name: 'Kerala',
+      pokemon: [
+        {value: 'Kochi', viewValue: 'Kochi'},
+        {value: 'Kozhikode', viewValue: 'Kozhikode'},
+        {value: 'Thrissur', viewValue: 'Thrissur'},
+        {value: 'Malappuram', viewValue: 'Malappuram'},
+
+      ],
+    },
+    {
+      name: 'Madhya Pradesh',
+      pokemon: [
+        {value: 'Indore', viewValue: 'Indore'},
+        {value: 'Gwalior', viewValue: 'Gwalior'},
+        {value: 'Jabalpur', viewValue: 'Jabalpur'},
+        {value: 'Ujjain', viewValue: 'Ujjain'},
+        {value: 'Sagar', viewValue: 'Sagar'},
+      ],
+    },
+    {
+      name: 'Maharashtra',
+      pokemon: [
+        {value: 'Pune', viewValue: 'Pune'},
+        {value: 'Nagpur', viewValue: 'Nagpur'},
+        {value: 'Nashik', viewValue: 'Nashik'},
+        {value: 'Aurangabad', viewValue: 'Aurangabad'},
+        {value: 'Solapur', viewValue: 'Solapur'},
+      ],
+    },
+    {
+      name: 'Manipur',
+      pokemon: [
+        {value: 'Bishnupur', viewValue: 'Bishnupur'},
+        {value: 'Ukhrul', viewValue: 'Ukhrul'},
+        {value: 'Tamenglong', viewValue: 'Tamenglong'},
+        {value: 'Chandel', viewValue: 'Chandel'},
+        {value: 'Senapati', viewValue: 'Senapati'},
+      ],
+    },
+    {
+      name: 'Meghalaya',
+      pokemon: [
+        {value: 'Cherrapunji', viewValue: 'Cherrapunji'},
+        {value: 'Tura', viewValue: 'Tura'},
+        {value: 'Jowai', viewValue: 'Jowai'},
+        {value: 'Baghmara', viewValue: 'Baghmara'},
+        {value: 'Nongpoh', viewValue: 'Nongpoh'},
+      ],
+    },
+    {
+      name: 'Mizoram',
+      pokemon: [
+        {value: 'Lunglei', viewValue: 'Lunglei'},
+        {value: 'Serchhip', viewValue: 'Serchhip'},
+        {value: 'Champhai', viewValue: 'Champhai'},
+        {value: 'Tuipang', viewValue: 'Tuipang'},
+        {value: 'Mamit', viewValue: 'Mamit'},
+      ],
+    },
+    {
+      name: 'Nagaland',
+      pokemon: [
+        {value: 'Tuensang', viewValue: 'Tuensang'},
+        {value: 'Zunheboto', viewValue: 'Zunheboto'},
+        {value: 'Mokokchung', viewValue: 'Mokokchung'},
+        {value: 'Kiphire Sadar', viewValue: 'Kiphire Sadar'},
+        {value: 'Phek', viewValue: 'Phek'},
+      ],
+    },
+    {
+      name: 'Odisha',
+      pokemon: [
+        {value: 'Rourkela', viewValue: 'Rourkela'},
+        {value: 'Cuttack', viewValue: 'Cuttack'},
+        {value: 'Brahmapur', viewValue: 'Brahmapur'},
+        {value: 'Puri', viewValue: 'Puri'},
+        {value: 'Sambalpur', viewValue: 'Sambalpur'},
+      ],
+    },
+    {
+      name: 'Punjab',
+      pokemon: [
+        {value: 'Amritsar', viewValue: 'Amritsar'},
+        {value: 'Jalandhar', viewValue: 'Jalandhar'},
+        {value: 'Ludhiana', viewValue: 'Ludhiana'},
+        {value: 'Patiala', viewValue: 'Patiala'},
+        {value: 'Kapurthala', viewValue: 'Kapurthala'},
+      ],
+    },
+    {
+      name: 'Rajasthan',
+      pokemon: [
+        {value: 'Bikaner', viewValue: 'Bikaner'},
+        {value: 'Jaisalmer', viewValue: 'Jaisalmer'},
+        {value: 'Jodhpur', viewValue: 'Jodhpur'},
+        {value: 'Udaipur', viewValue: 'Udaipur'},
+        {value: 'Ajmer', viewValue: 'Ajmer'},
+      ],
+    },
+    {
+      name: 'Sikkim',
+      pokemon: [
+        {value: 'Namchi', viewValue: 'Namchi'},
+        {value: 'Gyalshing', viewValue: 'Gyalshing'},
+        {value: 'Mangan', viewValue: 'Mangan'},
+        {value: 'Rabdentse', viewValue: 'Rabdentse'},
+
+      ],
+    },
+    {
+      name: 'Tamil Nadu',
+      pokemon: [
+        {value: 'Tiruchirappalli', viewValue: 'Tiruchirappalli'},
+        {value: 'Madurai', viewValue: 'Madurai'},
+        {value: 'Erode', viewValue: 'Erode'},
+        {value: 'Vellore', viewValue: 'Vellore'},
+        {value: 'Coimbatore', viewValue: 'Coimbatore'},
+      ],
+    },
+    {
+      name: 'Telangana',
+      pokemon: [
+        {value: 'Warangal', viewValue: 'Warangal'},
+        {value: ' Nizamabad ', viewValue: ' Nizamabad '},
+        {value: ' Karimnagar', viewValue: ' Karimnagar'},
+        {value: 'Adilabad', viewValue: 'Adilabad'},
+        {value: 'Khammam', viewValue: 'Khammam'},
+      ],
+    },
+{
+      name: 'Tripura',
+      pokemon: [
+        {value: 'Amarpur', viewValue: '	Amarpur'},
+        {value: ' Kumarghat ', viewValue: '  Kumarghat'},
+        {value: 'Udaipur,', viewValue: 'Udaipur,'},
+        {value: ' Gakulnagar', viewValue: 'Gakulnagar'},
+        {value: 'Kunjaban', viewValue: 'Kunjaban'},
+      ],
+    },
+{
+      name: 'Uttar Pradesh',
+      pokemon: [
+        {value: 'Noida', viewValue: 'Noida'},
+        {value: ' Varanasi', viewValue: ' Varanasi'},
+        {value: 'Allahabad,', viewValue: 'Allahabad,'},
+        {value: 'Agra', viewValue: 'Agra'},
+        {value: 'Kanpur', viewValue: ' Kanpur '},
+      ],
+    },
+{
+      name: 'Uttarakhand',
+      pokemon: [
+        {value: 'Haridwar', viewValue: 'Haridwar'},
+        {value: 'Roorkee', viewValue: 'Roorkee'},
+        {value: 'Rishikesh,', viewValue: 'Rishikesh,'},
+        {value: 'Kashipur', viewValue: ' Kashipur'},
+        {value: 'Haldwani', viewValue: ' Haldwani '},
+      ],
+    },
+{
+      name: 'West Bengal',
+      pokemon: [
+        {value: 'Darjeeling', viewValue: 'Darjeeling'},
+        {value: 'Siliguri', viewValue: 'Siliguri'},
+        {value: 'Asansol', viewValue: 'Asansol'},
+        {value: 'Howrah', viewValue: 'Howrah'},
+        {value: 'Durgapur', viewValue: 'Durgapur'},
+      ],
+    },
+  ];
+
 }
 interface person_title {
   value: string;
@@ -88,4 +393,17 @@ interface blood_group {
 interface state {
   value: string;
   viewValue: string;
+
+}
+
+interface Pokemon {
+  value: string;
+  viewValue: string;
+}
+
+interface PokemonGroup {
+  disabled?: boolean;
+  name: string;
+  pokemon: Pokemon[];
+  
 }
